4b07c70030031ef8087ce9baa2c6cebe7c5bbd01
jevgenij-p <eugene.pankov@gmail.com>
2024-12-10 20:25:05 +0200

    method message() called

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -23,4 +23,8 @@ describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
   });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
 });
