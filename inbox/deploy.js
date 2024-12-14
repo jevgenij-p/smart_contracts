e3e5b4658e5a8571e197b9428c985c854a1e000b
jevgenij-p <eugene.pankov@gmail.com>
2024-12-14 23:18:15 +0200

    added deployment to Sepolia

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,23 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require("./compile");
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+    'fee ankle prize kiss lamp spoon crowd alone success coyote maid among',
+    'https://sepolia.infura.io/v3/435c105af4da45a8acc0d1e91c6b6b58'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+  
+    console.log("Attempting to deploy from account", accounts[0]);
+  
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ["Hi there!"] })
+      .send({ gas: "1000000", from: accounts[0] });
+  
+    console.log("Contract deployed to", result.options.address);
+    provider.engine.stop();
+  };
+  deploy();
