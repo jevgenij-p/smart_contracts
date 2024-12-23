73125b3adf5c518ae8253e100d9c551347a8f600
jevgenij-p <eugene.pankov@gmail.com>
2024-12-23 13:47:05 +0200

    added tests

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -1,9 +1,9 @@
 const assert = require("assert");
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const { abi, evm } = require("../compile");
+const { interface, bytecode } = require("../compile");
 
 let lottery;
 let accounts;
@@ -18,5 +18,19 @@ beforeEach(async () => {
 describe("Lottery Contract", () => {
   it("deploys a contract", () => {
     assert.ok(lottery.options.address);
+  });
+
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
   });
 });
